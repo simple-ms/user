b131fc5983123b77bd5b895e651792cae7ea2c0a
Sabuhi <sabuhi@mail>
2025-11-29 17:31:59 +0400

    Remove index from username and email

--- app/models.py
+++ app/models.py
@@ -5,6 +5,6 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
